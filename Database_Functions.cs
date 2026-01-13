26719bac167e66353745d6f00703ce28c6c96eb4
George D. Rodakino <57292409+gdroda@users.noreply.github.com>
2026-01-14 00:54:23 +0200

    Add project files.

---
+++ Database_Functions.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.Data.SqlClient;
+
+
+namespace flashcards
+{
+    internal class Database_Functions
+    {
+        static void EstablishConnection(string str)
+        {
+            SqlConnection connection = new("Server=(localdb)\\MSSQLLocalDB;Integrated security=SSPI;database=flashcardDB;Trusted_Connection=true;");
+            SqlCommand command = new(str, connection);
+            try
+            {
+                connection.Open();
+                command.ExecuteNonQuery();
+                Console.WriteLine("Table Created.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error connecting to database...{ex}");
+            }
+            finally
+            {
+                if (connection.State == System.Data.ConnectionState.Open)
+                {
+                    connection.Close();
+                }
+            }
+        }
+
+        public static void CreateDefaultTables()
+        {
+            string stra = "IF OBJECT_ID('Stacks') is null CREATE TABLE Stacks (Id INT PRIMARY KEY IDENTITY, Name VARCHAR(64), StackId INT, FOREIGN KEY (StackId) REFERENCES Stacks(Id))";
+            EstablishConnection(stra);
+            string strb = "IF OBJECT_ID('Flashcards') is null CREATE TABLE Flashcards (Id INT PRIMARY KEY IDENTITY, Front VARCHAR(64), Back VARCHAR(64), FlashcardId INT, FOREIGN KEY (FlashcardId) REFERENCES Flashcards(Id))";
+            EstablishConnection(strb);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using flashcards;
+using Microsoft.Data.SqlClient;
+
+namespace Flashcards
+{
+    internal class Flashcards
+    {
+        static void Main(string[] args)
+        {
+            CreateDatabase();
+        }
+
+        static void CreateDatabase()
+        {
+            string str;
+            string check_str;
+            SqlConnection connection = new("Server=(localdb)\\MSSQLLocalDB;Integrated security=SSPI;database=master;Trusted_Connection=true;");
+
+            check_str = "IF NOT EXISTS(SELECT name FROM master.sys.databases WHERE name = N'flashcardDB')";
+            str = "CREATE DATABASE flashcardDB ON PRIMARY " +
+                "(NAME = flashcard_DB, " +
+                "FILENAME = 'C:\\SQLData\\Flashcard\\flashcardDB.mdf', " +
+                "SIZE = 5MB, MAXSIZE = 20MB, FILEGROWTH = 5MB)" +
+                "LOG ON (NAME = flashcardDB_Log, " +
+                "FILENAME = 'C:\\SQLDATA\\Flashcard\\flashcardLog.ldf', " +
+                "SIZE = 2MB, MAXSIZE = 5MB, FILEGROWTH = 2MB)";
+            
+
+            SqlCommand command = new(check_str + str, connection);
+            try
+            {
+                connection.Open();
+                command.ExecuteNonQuery();
+                Console.WriteLine("Database created successfully.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error creating database...{ex}");
+            }
+            finally
+            {
+                if (connection.State == System.Data.ConnectionState.Open)
+                {
+                    connection.Close();
+                }
+            }
+            Database_Functions.CreateDefaultTables();
+        }
+    }
+}
